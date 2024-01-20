a496efe9ab99749a0c1cfdd3c28cb4df6cba9fe5
KimRass <rmx1000@yonsei.ac.kr>
2024-01-20 13:58:55 +0900

    Doint EDA

--- eda.py
+++ eda.py
@@ -1,3 +1,6 @@
+# References:
+    # https://kaggle-kr.tistory.com/17?category=868316
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,12 +8,13 @@ import seaborn as sns
 import missingno as msno
 
 plt.style.use("seaborn-v0_8")
-sns.set(font_scale=2.5)
+sns.set(font_scale=1)
 
 pd.options.display.max_columns = 12
 pd.options.display.width = 70
 
 np.set_printoptions(linewidth=70)
+
 
 train_df = pd.read_csv("/Users/jongbeomkim/Desktop/workspace/Kaggle-Titanic/dataset/train.csv")
 
@@ -25,6 +29,10 @@ plt.tight_layout()
 plt.show()
 
 # "Sex" column
+fig, axes = plt.subplots(1, 1, figsize=(7, 7))
+sns.countplot(ax=axes, data=train_df, x="Sex", hue="Survived")
+plt.tight_layout()
+plt.show()
 
 # "Age" column
 age_cnts = train_df["Age"].value_counts().to_dict()
@@ -47,4 +55,24 @@ train_df[["Pclass", "Survived"]]
 ct = pd.crosstab(index=train_df["Pclass"], columns=train_df["Survived"])
 ct["total"] = ct[0] + ct[1]
 ct["ratio"] = ct[1] / ct["total"]
-ct
+print(ct)
+
+fig, axes = plt.subplots(1, 1, figsize=(7, 7))
+sns.countplot(ax=axes, data=train_df, x="Pclass", hue="Survived")
+plt.tight_layout()
+plt.show()
+
+# "Pclass" and "Sex" column
+# fig, axes = plt.subplots(1, 1, figsize=(7, 7))
+sns.catplot(data=train_df, x="Pclass", y="Survived", hue="Sex", kind="point", hue_order=[0, 1])
+plt.tight_layout()
+plt.show()
+
+# "Fare" column
+sns.histplot(train_df["Fare"])
+plt.tight_layout()
+plt.show()
+
+sns.histplot(train_df["Fare"].apply(np.log))
+plt.tight_layout()
+plt.show()
